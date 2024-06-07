eb9a03b585361a86c3b2259bef6f17b326f7355a
LittleHero05 <isabellaporraslopez@gmail.com>
2024-06-07 15:13:23 -0400

    finished basic calculator

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const { displayvideo } = require("googleapis/build/src/apis/displayvideo");
-
 n1 = 0;
 n2 = 0;
 operand = "";
 
+
 display = document.querySelector("#display");
 
 function add(num1, num2) {
-    return num1 + num2;
+    return num1 + num2
 }
 
 function multiply(num1, num2) {
@@ -22,6 +21,47 @@ function subtract(num1, num2) {
     return num1 - num2
 }
 
-function appendChar(char) {
-    display.text
+function operate(num1, num2, operand) {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    if(operand == "+") {
+        return add(num1, num2)
+    } else if (operand == "*") {
+        return multiply(num1, num2)
+    } else if (operand == "/") {
+        if(num2 === 0) {
+            return 'expected error';;
+        } else {
+        return divide(num1, num2);
+    } }
+    else {
+        return subtract(num1, num2)
+    }
+}
+
+function appendChar(val) {
+    if (val == '*' || val == '/' || val == '-' || val == '+') {
+        n1 = display.value;
+        operand = val;
+        display.value = "";
+    } else {
+        display.value += val;
+    }
+}
+
+function calculate() {
+    n2 = display.value;
+    display.value = "";
+    display.value = operate(n1, n2, operand);
+}
+
+function clearDisplay() {
+    display.value = "";
+    n1 = 0;
+    n2 = 0;
+    operand = '';
+}
+
+function deleteChar() {
+    display.value = display.value.substring(0, display.value.length - 1);
 }
